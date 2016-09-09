5353f231d7ce9fa397851ae91b32ef476ad114fb
David Cortes <dcortes22@gmail.com>
2016-09-09 15:17:27 -0600

    Agregado el websocket

--- Backend/CLEApi/config/routes.rb
+++ Backend/CLEApi/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+  
   resources :drinks
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
