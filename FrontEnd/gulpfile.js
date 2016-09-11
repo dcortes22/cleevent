e1ef8c3491aabd10e0088831a27c7546ad4a1791
Daniel Cortes Saenz <dcortes92@hotmail.com>
2016-09-10 20:52:51 -0600

    chore(build): Improved gulp sass task
    Added bootsrap and responsive mixins

--- FrontEnd/gulpfile.js
+++ FrontEnd/gulpfile.js
@@ -5,20 +5,47 @@ var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
+var source = 'styles/',
+    dest = 'dist/';
+
+// Bootstrap scss source
+var bootstrapSass = {
+  in: './node_modules/bootstrap-sass/'
+};
+
+// Bootstrap fonts source
+// var fonts = {
+//   in: [source + 'fonts/*.*', bootstrapSass.in + 'assets/fonts/**/*'],
+//   out: dest + 'fonts/'
+// };
+
+// Our scss source folder: .scss files
+var scss = {
+  in: source + 'general.scss',
+  out: dest + 'css/',
+  watch: source + 'styles/**/*',
+  sassOpts: {
+    outputStyle: 'nested',
+    precison: 3,
+    errLogToConsole: true,
+    includePaths: [bootstrapSass.in + 'assets/stylesheets']
+  }
+};
+
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
     server: {}
   });
 
-  gulp.watch("./styles/*.scss", ['sass']);
+  gulp.watch("styles/*.scss", ['sass']);
   gulp.watch("index.html").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function() {
-  return gulp.src("./styles/**/*.scss")
-    .pipe(sass())
+  return gulp.src(scss.in)
+    .pipe(sass(scss.sassOpts))
     .pipe(concat('main.css'))
-    .pipe(gulp.dest("./dist/css/"))
+    .pipe(gulp.dest(scss.out))
     .pipe(browserSync.stream());
 });
 
