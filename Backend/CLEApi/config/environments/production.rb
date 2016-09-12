84f82351e4b2422aec87ec706fea0c94925e7a66
David Cortes <dcortes22@gmail.com>
2016-09-12 11:18:57 -0600

    Agregando deploys a heroku

--- Backend/CLEApi/config/environments/production.rb
+++ Backend/CLEApi/config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://cleapi.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://cleapi.herokuapp.com', 'http://cleapi.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
