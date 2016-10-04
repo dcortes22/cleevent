75d1cd4f6ed3f39dca2d6ec0bb2302475e57867c
David Cortes <dcortes22@gmail.com>
2016-10-04 10:13:37 -0600

    Adding new endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://cleapi.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://cleapi.herokuapp.com', 'http://cleapi.herokuapp.com', 'https://ordercle.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://cleapi.herokuapp.com', 'http://cleapi.herokuapp.com', 'https://ordercle.herokuapp.com/', 'https://whispering-sea-66329.herokuapp.com/', 'http://whispering-sea-66329.herokuapp.com/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
